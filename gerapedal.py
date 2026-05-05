ed6dcabc1908f7b8245c75aea5bf05cf293d83a8
lucassabino96 <lsabino2012@gmail.com>
2026-05-05 15:28:07 -0300

    Update gerapedal.py

--- gerapedal.py
+++ gerapedal.py
@@ -32,7 +32,8 @@ horario = st.text_input("Horário", "06:00")
 
 vagas = st.selectbox("Número de vagas", list(range(1, 31)))
 
-data_hoje = date.today().strftime("%d/%m/%Y")
+data = st.date_input("Data do pedal", value=date.today())
+data_formatada = data.strftime("%d/%m/%Y")
 
 # =========================
 # GERAR LISTA DE VAGAS
@@ -50,12 +51,20 @@ if st.button("Gerar texto"):
 
 📍 {destino}
 
-📅 {data_hoje}
+📅 {data_formatada}
 ⏰ {horario}
 📌 {local}
 
 Confirmados:
-{lista_vagas}
+numeros_emoji = [
+    "1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣",
+    "🔟","1️⃣1️⃣","1️⃣2️⃣","1️⃣3️⃣","1️⃣4️⃣","1️⃣5️⃣",
+    "1️⃣6️⃣","1️⃣7️⃣","1️⃣8️⃣","1️⃣9️⃣","2️⃣0️⃣",
+    "2️⃣1️⃣","2️⃣2️⃣","2️⃣3️⃣","2️⃣4️⃣","2️⃣5️⃣",
+    "2️⃣6️⃣","2️⃣7️⃣","2️⃣8️⃣","2️⃣9️⃣","3️⃣0️⃣"
+]
+
+lista_vagas = "\n".join([f"{numeros_emoji[i]} _____________" for i in range(vagas)])
 """
 
     st.text_area("Texto pronto", texto, height=300)
